ea3fc72c624dc031e129baea07dd62217786b90a
Vinicius Campbell dos Santos <campbeu0@rocketmail.com>
2026-07-08 21:34:26 -0300

    Primeira versão aula de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EpiList } from './epi-list/epi-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EpiList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('seguranca-frontend');
+  // protected readonly title = signal('Aula de Angular Turma 2026.1');
+  titulo = "Sistema de controle de EPI's";
+  subtitulo = "Gestão de Segurança do Trabalho";
+  descricao = "Aplicação para controle, monitoramento e conformidade de Equipamentos de Proteção Individual (EPI's).";
 }
